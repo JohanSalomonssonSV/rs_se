b5d76e4e7de74bab41cd9e7e7388e1097a74e1f5
JohanSalomonssonSV <johan.salomonsson88@gmail.com>
2023-05-02 12:48:52 +0200

    commit2023-05-02 12:48:52

--- r/RS_script.R
+++ r/RS_script.R
@@ -31,7 +31,13 @@ readr::write_csv(RS_df,file=paste0("C:/Users/johan/Documents/github/rs_se/data/r
 git2r::add(path = c("r/RS_script.R", "data/rs_se.csv"),#"C:/Users/johan/Documents/github/rs_se"
            )
 git2r::commit(message = paste0("commit", Sys.time()))
-git2r::push()
+git2r::push("C:/Users/johan/Documents/github/rs_se",
+  credentials = cred_user_pass( username = Sys.getenv("GH_Name") 
+                                , password = Sys.getenv("GH_PASS")  
+  )      
+  
+  
+)
 
 repo <- git2r::repository("https://github.com/JohanSalomonssonSV/rs_se.git")
 
