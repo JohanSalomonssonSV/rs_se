c5493dbbd4d57d0b6a2ea75583328f5a894eeb95
JohanSalomonssonSV <johan.salomonsson88@gmail.com>
2023-05-02 12:37:19 +0200

    commit2023-05-02 12:37:19

--- r/RS_script.R
+++ r/RS_script.R
@@ -13,7 +13,7 @@ score_function<-\(.,col){
     ) |> 
     rename("RS_{{col}}":=rank)
 }
-
+ 
 RS_df<-t |> select(description, name, perf_1m, perf_3m, perf_6m, perf_ytd, perf_y) |>
   mutate(symbol_yf=str_replace(paste0(name,".ST"), "_","-"),
          stock=str_to_title(description)) |> 
@@ -24,14 +24,30 @@ RS_df<-t |> select(description, name, perf_1m, perf_3m, perf_6m, perf_ytd, perf_
   score_function(perf_y) |>
   select(stock, symbol=name,symbol_yf , starts_with("RS_")) |> 
   arrange(-RS_perf_y) |> 
-  mutate(date=today())
+  mutate(date=Sys.time())
 
 readr::write_csv(RS_df,file=paste0("C:/Users/johan/Documents/github/rs_se/data/rs_se.csv"))
 
-git2r::add(path = "C:/Users/johan/Documents/github/rs_se")
+git2r::add(path = c("r/RS_script.R", "data/rs_se.csv"),#"C:/Users/johan/Documents/github/rs_se"
+           )
 git2r::commit(message = paste0("commit", Sys.time()))
+git2r::push()
 
-git2r::push(path = "C:/Users/johan/Documents/github/rs_se")
+repo <- git2r::repository("https://github.com/JohanSalomonssonSV/rs_se.git")
+
+git2r::config( user.name="JohanSalomonssonSV", user.email="johan.salomonsson88@gmail.com")
+
+
+
+# library(git2r)
+# path_bare <- tempfile(pattern="git2r-")
+# path_repo <- tempfile(pattern="git2r-")
+# dir.create(path_bare)
+# dir.create(path_repo)
+# repo_bare <- init(path_bare, bare = TRUE)
+
+#git2r::cred
+#git2r::push(path = "C:/Users/johan/Documents/github/rs_se")
 
 #library(git2rdata)
 
