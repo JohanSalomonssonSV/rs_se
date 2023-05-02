f6c7d4a7a5898399d08927d7130c1f0c2d9b3f09
JohanSalomonssonSV <johan.salomonsson88@gmail.com>
2023-05-02 13:35:25 +0200

    test

--- r/RS_script.R
+++ r/RS_script.R
@@ -35,50 +35,18 @@ git2r::add(path = c("r/RS_script.R", "data/rs_se.csv"),#"C:/Users/johan/Document
 git2r::commit(message = paste0("commit", Sys.time()))
 
 git2r::push(getwd(),
-  credentials = git2r::cred_user_pass( username = "JohanSalomonssonSV",
-                                 password = Sys.getenv("GH_PASS")  
-  )      
+  credentials = git2r::cred_user_pass( username = Sys.getenv("GH_Name"),
+                                 password = Sys.getenv("GH_PASS") ),
+  set_upstream = TRUE
   
   
 )
 
-# repo <- git2r::repository("https://github.com/JohanSalomonssonSV/rs_se.git")
-# 
-# git2r::config( user.name="JohanSalomonssonSV", user.email="johan.salomonsson88@gmail.com")
+# git2r::push(getwd(),
+#             credentials = git2r::cred_env("GH_Name","GH_PASS") 
+#             )      
+#             
+           
 
 
-
-# library(git2r)
-# path_bare <- tempfile(pattern="git2r-")
-# path_repo <- tempfile(pattern="git2r-")
-# dir.create(path_bare)
-# dir.create(path_repo)
-# repo_bare <- init(path_bare, bare = TRUE)
-
-#git2r::cred
-#git2r::push(path = "C:/Users/johan/Documents/github/rs_se")
-
-#library(git2rdata)
-
-# root <- "~/rs_se" 
-# write_vc(my_data, file = "rel_path/filename", root = root)
-# read_vc(file = "rel_path/filename", root = root)
-# root <- git2r::repository("~/my_git_repo") # git repository
-
-# Using a git repository
-# library(git2rdata)
-# 
-# repo <- repository("https://github.com/JohanSalomonssonSV/rs_se.git")
-# pull(repo)
-# write_vc(my_data, file = "rel_path/filename", root = repo, stage = TRUE)
-# commit(repo, "My message")
-# push(repo)
-# read_vc(file = "rel_path/filename", root = repo)
-
-# repo <- repository("~/my_git_repo")
-# pull(repo)
-# write_vc(my_data, file = "rel_path/filename", root = repo, stage = TRUE)
-# commit(repo, "My message")
-# push(repo)
-# read_vc(file = "rel_path/filename", root = repo)
 
